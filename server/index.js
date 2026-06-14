681fa79bcf1656533bbb58a98977231e059520c9
Kashif2004K <blackwatch4321@gmail.com>
2026-06-14 23:30:27 +0500

    fix railway port issue

--- server/index.js
+++ server/index.js
@@ -65,7 +65,7 @@ app.get("/api/test", (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => {
   console.log("Server running on port", PORT);
