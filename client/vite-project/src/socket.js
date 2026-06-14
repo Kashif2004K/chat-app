bbc16c9136652ba0c5d33cdc7a1c6d930bd2cdf7
Kashif2004K <blackwatch4321@gmail.com>
2026-06-14 22:46:22 +0500

    Update socket configuration

--- client/vite-project/src/socket.js
+++ client/vite-project/src/socket.js
@@ -1,3 +1,3 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:5000");
+export const socket = io("https://chat-app-production-2351.up.railway.app");
--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ let onlineUsers = {};
 // Socket setup
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "*", // temporary for deployment
     methods: ["GET", "POST"],
   },
 });
@@ -73,6 +73,8 @@ app.get("/api/test", (req, res) => {
 });
 
 // Start server
-server.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+  console.log("Server running on port", PORT);
 });
